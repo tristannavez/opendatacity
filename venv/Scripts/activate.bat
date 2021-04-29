47126de0598f190d6745199dbd2c4705257fbba0
Jourdain KIMB'S <jourdainkimbembe530@yahoo.fr>
2021-04-29 02:19:44 +0200

    insert bdd

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\oriol\PycharmProjects\opendatacity\venv
+set VIRTUAL_ENV=E:\opendatacity\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
